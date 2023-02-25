f073f980fc2583953c96c980fd2fee0db4ffa865
Muhammad Qasim <mqtech25@gmail.com>
2023-02-25 18:48:52 +0500

    update in style

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ function selectItem(e) {}
 tabItems.forEach((item) => {
   item.addEventListener("click", selectItem);
 });
+
+const searchBtn= document.getElementById('search-btn');
+searchBtn.addEventListener('click',(event)=>{
+  event.preventDefault();
+const slidersec= document.getElementById('slider-sec');
+// console.log(slidersec.scrollHeight);
+window.scrollTo({
+  top: slidersec.scrollHeight,
+  left: 0,
+  behavior: 'smooth'
+});
+})
